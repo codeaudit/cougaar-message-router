75d2809a1efb02bca3b285886b9be282098a8507
dave <dave>
2004-06-25 19:45:19 +0000

    *** empty log message ***

--- cppmessagerouter/messagerouter.sh
+++ cppmessagerouter/messagerouter.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 while (1)
-  /usr/local/sbin/messageserver -w >>& /var/log/messagerouter.log &
+  /usr/local/sbin/messageserver -i >> /var/log/messagerouter.log &
   echo "$!" > messagerouter.pid
   while (-e /proc/$!) 
     sleep 5
