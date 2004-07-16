7ceacd50f932af9aa7f06c3ca4455ef3bec525eb
dave <dave>
2004-07-16 14:38:37 +0000

    added line to save log fiel in case of crash

--- cppmessagerouter/messagerouter.sh
+++ cppmessagerouter/messagerouter.sh
@@ -1,10 +1,11 @@
 #!/bin/csh
 
 while (1)
-  /usr/local/sbin/messageserver -w >> /var/log/messagerouter.log &
+  /usr/local/sbin/messageserver -d >> /var/log/messagerouter.log &
   echo "$!" > messagerouter.pid
   while (-e /proc/$!) 
     sleep 5
   end   
   echo MESSAGEROUTER DIED >> /var/log/messagerouter.log
+  mv /var/log/messagerouter.log /var/log/messagerouter.crash.`date +%s`
 end
